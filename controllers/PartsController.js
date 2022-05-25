98b93203d051b552131625296221eceee5204e6b
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 20:41:20 +0600

    Last time Debugging for that bug

--- controllers/PartsController.js
+++ controllers/PartsController.js
@@ -1,17 +1,9 @@
-const { connect } = require('../client.js')
 const client = require('../client.js')
+const partsCollection = client.db('abacus-parts').collection('parts')
 
 exports.get_parts = async (req, res) => {
-  try {
-    await client.connect()
-    const partsCollection = client.db('abacus-parts').collection('parts')
-    const query = {}
-    const result = await partsCollection.find(query).toArray()
+  const query = {}
+  const result = await partsCollection.find(query).toArray()
 
-    res.send(result)
-  } catch (error) {
-    console.log(error)
-  } finally {
-    await client.close()
-  }
+  res.send(result)
 }
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const client = require(__dirname + '/client')
-const userController = require(__dirname + '/controllers/UserController')
-const partsController = require(__dirname + '/controllers/PartsController')
+const client = require('./client')
+const userController = require('./controllers/UserController')
+const partsController = require('./controllers/PartsController')
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -15,19 +15,17 @@ app.get('/', (req, res) => {
   res.send('Manufacturer Website Server is Running')
 })
 
-app.put('/user/:email', userController.update_user)
+const run = async () => {
+  try {
+    await client.connect()
+    app.put('/user/:email', userController.update_user)
 
-app.get('/part', partsController.get_parts)
+    app.get('/part', partsController.get_parts)
+  } finally {
+  }
+}
 
-// const run = async () => {
-//   try {
-//     await client.connect()
-
-//   } finally {
-//   }
-// }
-
-// run().catch(console.dir)
+run().catch(console.dir)
 
 app.listen(port, () => {
   console.log('Server is Ruuning on port, ', port)
