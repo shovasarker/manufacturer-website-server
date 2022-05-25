4c612d13fff0074a3b3001d42fc80c7f58fb1f7b
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 19:35:30 +0600

    New Controller added PartsController for route part to get all the Parts from the db

---
+++ controllers/PartsController.js
@@ -0,0 +1,10 @@
+const client = require('../client.js')
+
+const partsCollection = client.db('abacus-parts').collection('parts')
+
+exports.get_parts = async (req, res) => {
+  const query = {}
+  const result = await partsCollection.find(query).toArray()
+
+  res.send(result)
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 const client = require('./client')
 const userController = require('./controllers/UserController')
+const partsController = require('./controllers/PartsController')
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -14,17 +15,12 @@ app.get('/', (req, res) => {
   res.send('Manufacturer Website Server is Running')
 })
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fiys1.mongodb.net/?retryWrites=true&w=majority`
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// })
-
 const run = async () => {
   try {
     await client.connect()
     app.put('/user/:email', userController.update_user)
+
+    app.get('/part', partsController.get_parts)
   } finally {
   }
 }
