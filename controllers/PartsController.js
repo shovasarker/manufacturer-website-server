9a7db22b74d182e63ce8062709aeb31e24319803
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 20:49:02 +0600

    small Part Code Refactored in PartContoller

--- controllers/PartsController.js
+++ controllers/PartsController.js
@@ -1,11 +1,16 @@
 const client = require('../client.js')
 
+const getCollection = async () => {
+  await client.connect()
+  const partsCollection = client.db('abacus-parts').collection('parts')
+  return partsCollection
+}
+
 exports.get_parts = async (req, res) => {
   try {
-    await client.connect()
-    const partsCollection = client.db('abacus-parts').collection('parts')
+    const collection = await getCollection()
     const query = {}
-    const result = await partsCollection.find(query).toArray()
+    const result = await collection.find(query).toArray()
 
     res.send(result)
   } catch (error) {
