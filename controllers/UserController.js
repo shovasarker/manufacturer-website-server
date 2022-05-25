397d9c3e38294fa237862fb0cb47da8345d4f3fe
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 20:53:30 +0600

    UserCollection Code Also debugged for heroku deployment

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,28 +1,38 @@
 const client = require('../client.js')
 const jwt = require('jsonwebtoken')
-
-const userCollection = client.db('abacus-parts').collection('users')
+const getCollection = async () => {
+  await client.connect()
+  const userCollection = client.db('abacus-parts').collection('users')
+  return userCollection
+}
 
 exports.update_user = async (req, res) => {
-  const user = req.body
-  const { email } = req.params
-  const filter = { email: email }
-  const options = {
-    upsert: true,
+  try {
+    const collection = await getCollection()
+    const user = req.body
+    const { email } = req.params
+    const filter = { email: email }
+    const options = {
+      upsert: true,
+    }
+
+    const updatedDoc = {
+      $set: {
+        ...user,
+        lastLoggedIn: new Date(),
+      },
+    }
+
+    const result = await collection.updateOne(filter, updatedDoc, options)
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '1d',
+    })
+
+    res.send({ result, accessToken: accessToken })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await client.close()
   }
-
-  const updatedDoc = {
-    $set: {
-      ...user,
-      lastLoggedIn: new Date(),
-    },
-  }
-
-  const result = await userCollection.updateOne(filter, updatedDoc, options)
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: '1d',
-  })
-
-  res.send({ result, accessToken: accessToken })
 }
