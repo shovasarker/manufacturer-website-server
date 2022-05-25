17849ed97c04a27b46bcca72162bba3c710ad8bb
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 20:36:33 +0600

    Debugging Continues

--- controllers/PartsController.js
+++ controllers/PartsController.js
@@ -1,11 +1,10 @@
 const { connect } = require('../client.js')
 const client = require('../client.js')
 
-const partsCollection = client.db('abacus-parts').collection('parts')
-
 exports.get_parts = async (req, res) => {
   try {
     await client.connect()
+    const partsCollection = client.db('abacus-parts').collection('parts')
     const query = {}
     const result = await partsCollection.find(query).toArray()
 
