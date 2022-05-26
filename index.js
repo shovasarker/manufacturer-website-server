56801591a64d93fd0ba6bdb6c20c66a1681c34c5
ShovaSarker <shovasarker01@gmail.com>
2022-05-26 22:48:08 +0600

    Added some comments in the index.js file

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/', (req, res) => {
   res.send('Manufacturer Website Server is Running')
 })
 
+// Users and Admin Routes
 app.put('/user/:email', userController.update_user)
 app.get('/user/:email', VerifyJWT, userController.get_user_by_email)
 app.get('/user', VerifyJWT, VerifyAdmin, userController.get_users)
@@ -27,6 +28,7 @@ app.patch('/user/profile/:email', VerifyJWT, userController.update_user_profile)
 app.get('/admin/:email', VerifyJWT, userController.check_admin)
 app.put('/user/admin/:email', VerifyJWT, VerifyAdmin, userController.make_admin)
 
+// Parts Related Routes
 app.get('/part', partsController.get_parts)
 app.get('/part/search', partsController.get_part_by_name)
 app.get('/part/:id', partsController.get_part_by_id)
@@ -38,10 +40,12 @@ app.delete(
 )
 app.post('/part', VerifyJWT, VerifyAdmin, partsController.add_part)
 
+// Reviews Related Routes
 app.post('/review', VerifyJWT, reviewsController.add_review)
 app.get('/review', reviewsController.get_reviews)
 app.get('/review/:email', VerifyJWT, reviewsController.get_reviews_by_email)
 
+// Order Related Routes
 app.get('/order', VerifyJWT, VerifyAdmin, ordersController.get_all_orders)
 app.post('/order', VerifyJWT, ordersController.add_new_order)
 app.get('/order/:email', VerifyJWT, ordersController.get_order_by_email)
@@ -55,6 +59,7 @@ app.patch(
 )
 app.delete('/order/:id', VerifyJWT, ordersController.delete_order_by_id)
 
+// Payment Intent Route
 app.post(
   '/create-payment-intent',
   VerifyJWT,
